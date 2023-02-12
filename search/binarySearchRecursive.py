509f705be94e8fb9e8580daab07857e522bade36
Francesco Pio Scognamiglio <francescopio.scognamiglio@4science.it>
2023-02-12 10:46:03 +0100

    improve logging on recursive binary search

--- search/binarySearchRecursive.py
+++ search/binarySearchRecursive.py
@@ -14,9 +14,8 @@ def binarySearchRecursive(A,x):
     c = math.floor(n/2)
 
     if (A[c] == x): return True # element found
-    print(n)
-    print(c)
     if (n == 1): return False # no element found
+
     if (A[c] > x): return binarySearchRecursive(A[0:c], x) # search into the n/2 left part of the array
     return binarySearchRecursive(A[c+1:n], x) # search into the n/2 right part of the array
 
@@ -26,7 +25,9 @@ foundCounter = 0
 # prepare a list of n sequential elements (ordered)
 list = [x for x in range(n)]
 for i in range (n):
-    v = binarySearchRecursive(list, random.randint(0, n*2))
+    x = random.randint(0, n*2)
+    print('searching ' + str(x) + ' ...')
+    v = binarySearchRecursive(list, x)
     if (v == True): foundCounter+=1
 
 print('the search average is: ' + str(foundCounter/n))
