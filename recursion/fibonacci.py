6dc5949085638f0a725fe5e54a2062f7ce42e4f2
Francesco Pio Scognamiglio <francescopio.scognamiglio@4science.it>
2023-02-17 16:44:31 +0100

    add iterative and optimized way to calculate Fibonacci's numbers

--- recursion/fibonacci.py
+++ recursion/fibonacci.py
@@ -10,4 +10,22 @@ def fibonacci(n):
     if n <= 1: return n
     return fibonacci(n-2) + fibonacci(n-1)
 
-print(fibonacci(30))
+def fibonacciIterative(n):
+    A = [0,1]
+    if n<=1: return A[n]
+    for i in range (2, n+1):
+        A.append(A[-1]+A[-2]) # A[-1] take last element
+                              # A[-2] take second to last element
+    return A[-1]
+
+def fibonacciIterativeOptimized(n):
+    f1 = 0
+    f2 = 1
+    if n<=1: return n
+    for i in range (2, n+1):
+        f1, f2 = f2, f1+f2
+    return f2
+
+#print(fibonacci(30))
+print(fibonacciIterative(50))
+print(fibonacciIterativeOptimized(50))
