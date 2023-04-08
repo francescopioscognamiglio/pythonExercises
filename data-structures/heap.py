2bcb8348698b4b3cd27afe924640cc810d00a6ff
Francesco Pio Scognamiglio <francescopio.scognamiglio@4science.it>
2023-04-08 18:16:38 +0200

    add heap data structure

---
+++ data-structures/heap.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  6 18:58:13 2023
+
+@author: scognamigliofrancescopio@gmail.com
+"""
+
+## remove the maximux (it is the first element: A[i])
+def extractMax(A, size):
+    # move the last element into the first
+    size -= 1
+    A[0] = A[size]
+    A.pop(size)
+    # fix the heap to satisfy this rule: each node is greater than or equal to its children
+    heapify(v, len(v), 0)
+
+def heapify(A, size, i):
+    # retrieve the node position
+    N = i
+    # retrieve the children positions
+    L, R = 2*i+1, 2*i+2
+    # check the maximum between current node and its children
+    if L < size and A[L] > A[N]:
+        N = L
+    if R < size and A[R] > A[N]:
+        N = R
+    if N == i: return
+    # switch the maximum with the current node
+    A[i], A[N] = A[N], A[i]
+
+    # call the procedure on the sub heap
+    heapify(A, size, N)
+
+def buildHeap(A, size):
+    # iterate on each parent node
+    for i in range((size-2)//2, -1, -1): # reverse for
+        heapify(A, size, i)
+
+
+v = [18, 16, 17, 12, 15, 10, 1, 8, 4, 14, 11, 3]
+print(v)
+extractMax(v, len(v))
+print(v)
