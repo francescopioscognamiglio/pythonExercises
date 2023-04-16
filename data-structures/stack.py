450d9787ead41cfcca326df829059c6d784134f6
Francesco Pio Scognamiglio <francescopio.scognamiglio@4science.it>
2023-04-16 15:12:30 +0200

    add stack data structure

---
+++ data-structures/stack.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 16 14:40:09 2023
+
+@author: scognamigliofrancescopio@gmail.com
+"""
+
+class Stack:
+    def __init__(self, key = None, next = None):
+        self.key = key
+        self.next = next
+
+def push(top, node):
+    node.next = top
+    return node
+
+def pop(top):
+    if top == None: return None, None # empty stack
+    newtop = top.next
+    top.next = None
+    return top, newtop
+
+
+top = Stack(5)
+top = push(top, Stack(11))
+top = push(top, Stack(3))
+top = push(top, Stack(63))
+node, top = pop(top)
+print(node.key)
+node, top = pop(top)
+print(node.key)
+node, top = pop(top)
+print(node.key)
+node, top = pop(top)
+print(node.key)
