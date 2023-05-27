3a4f14dbc5b302907ce85bcf795e84c36ed04df1
Francesco Pio Scognamiglio <francescopio.scognamiglio@4science.it>
2023-05-27 10:02:46 +0200

    update exercise: check if the given tree is a search binary tree via in order traversal (because in case of a search binary tree it gives an ordered array)

--- exercises/checkIfSearchBinaryTree.py
+++ exercises/checkIfSearchBinaryTree.py
@@ -24,6 +24,25 @@ def checkSBT(node, left = False):
     if left: return minr
     return maxl
 
+def inOrderTraversal(node, A):
+    if node == None: return
+    if node.left == None and node.right == None:
+        A.append(node.key)
+        return
+    inOrderTraversal(node.left, A)
+    A.append(node.key)
+    inOrderTraversal(node.right, A)
+
+def checkSBTViaSorting(node):
+    A = []
+    inOrderTraversal(node, A)
+    i = 0
+    while i < len(A):
+        if i+1 < len(A) and A[i] >= A[i+1]:
+            return False
+        i += 1
+    return True
+
 
 root = NodeSBT(10)
 root.left = NodeSBT(7)
@@ -33,9 +52,13 @@ root.right = NodeSBT(15)
 root.right.left = NodeSBT(12)
 root.right.left.right = NodeSBT(14)
 root.right.right = NodeSBT(20)
-print(check)
 checkSBT(root)
+assert check == True
+assert checkSBTViaSorting(root) == True
 print(check)
+print(checkSBTViaSorting(root))
+
+print()
 
 root = NodeSBT(3)
 root.left = NodeSBT(2)
@@ -45,6 +68,8 @@ root.right = NodeSBT(10)
 root.right.left = NodeSBT(6)
 root.right.left.right = NodeSBT(8)
 root.right.right = NodeSBT(20)
-print(check)
 checkSBT(root)
+assert check == False
+assert checkSBTViaSorting(root) == False
 print(check)
+print(checkSBTViaSorting(root))
