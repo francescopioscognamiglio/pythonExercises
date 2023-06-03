2d81087b8d19e4789131c9dfde11263e56038ad5
Francesco Pio Scognamiglio <francescopio.scognamiglio@4science.it>
2023-06-03 11:54:04 +0200

    add exercise: pre order traversal of a binary tree implemented via parents array

---
+++ exercises/preOrderTraversalInBinaryTreeImplementedViaParentsArray.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  3 11:18:53 2023
+
+@author: scognamigliofrancescopio@gmail.com
+"""
+
+def findRoot(A, P):
+    i = 0
+    while i < len(P):
+        if P[i] == None: return i
+        i += 1
+    return None
+
+def findChildren(A, P, i):
+    C = [None, None]
+    j, k = 0, 0
+    while j < len(P):
+        if P[j] == i:
+            C[k] = j
+            k += 1
+        j += 1
+    return C[0], C[1]
+
+def preOrderTraversal(A, P):
+    i = findRoot(A, P)
+    preOrderPrint(A, P, i)
+
+def preOrderPrint(A, P, i, h = 0):
+    if i == None: return
+    print(h*'-', A[i])
+    li, ri = findChildren(A, P, i)
+    preOrderPrint(A, P, li, h+1)
+    preOrderPrint(A, P, ri, h+1)
+
+
+A = [10, 5, 3]
+P = [None, 0, 0]
+preOrderTraversal(A, P)
+
+A = [7, 2, 10, 5, 4, 5, 3]
+P = [2, 2, None, 0, 1, 1, 0]
+print()
+preOrderTraversal(A, P)
